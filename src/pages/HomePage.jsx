12f672c4018254acaf6ec406737f540c82d93850
Charli311 <144750126+Charli311@users.noreply.github.com>
2025-02-20 01:27:43 -0600

    Route-fixing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,11 @@ const HomePage = () => {
           smartwatch y smartphone, permite a los árbitros registrar eventos de
           manera eficiente y precisa durante los partidos.
         </p>
-        <img src="\Images/Referee.png" alt="Árbitro" className="intro_image" />
+        <img
+          src="/whistle-time-landing/Images/Referee.png"
+          alt="Árbitro"
+          className="intro_image"
+        />
       </div>
 
       {/* Sección de motivos */}
@@ -43,13 +47,16 @@ const HomePage = () => {
       <div className="documents_section">
         <p className="documents_title">Documentos importantes</p>
         <div className="buttons">
-          <a href="\docs\Aviso-de-Privacidad-Integral_WhistleTime.pdf" download>
+          <a
+            href="/whistle-time-landing/docs/Aviso-de-Privacidad-Integral_WhistleTime.pdf"
+            download
+          >
             <button className="doc_button">
               Descargar Aviso de Privacidad
             </button>
           </a>
           <a
-            href="\docs\Términos-y-Condiciones-de-Uso_WhistleTime.pdf"
+            href="/whistle-time-landing/docs/Términos-y-Condiciones-de-Uso_WhistleTime.pdf"
             download
           >
             <button className="doc_button">
